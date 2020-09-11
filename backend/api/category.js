5e46f545f59a6891bfe0dd209b88a8ea91b1ae28
XandyGomes <x3.computadores@hotmail.com>
2020-09-11 19:42:22 -0300

    11.09.2020

---
+++ backend/api/category.js
@@ -0,0 +1,44 @@
+module.exports = app => {
+    const { existsOrError, notExistsOrError } = app.api.validation
+
+    const save = (req, res) => {
+        const category = { ...req.body }
+        if(req.params.id) category.id = req.params.id
+
+        try {
+            existsOrError(category.name, 'Nome não informado')
+        } catch(msg) {
+            return res.status(400).send(msg)
+        }
+
+        if(category.id) {
+            app.db('categories')
+                .update(category)
+                .where({ id: category.id })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        } else {
+            app.db('categories')
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
+        }
+    }
+
+    const remove = async (req, res) => {
+        try {
+            existsOrError(req.params.id, 'Codigo de Categoria não informado')
+
+            const subcategory = await app.db('categories')
+                .where({ parentId: req.params.id })
+
+                notExistsOrError(subcategory, 'Categoria possui subcategoria')
+
+                const articles = await app.db('articles')
+                    .where({ categoryId: req.params.id })
+                
+                notExistsOrError(articles, 'Categoria possui artigos')
+        }
+    }
+
+} 
