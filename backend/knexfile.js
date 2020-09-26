270f86daf2c91422aa5a3b1c77ebf9927f28c790
XandyGomes <x3.computadores@hotmail.com>
2020-09-25 22:22:34 -0300

    25.09.2020

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,8 @@
+const { db } = require('./.env')
+
 module.exports = {
   client: 'postgresql',
-  connection: {
-    database: 'conhecimento_noite',
-    user:     'postgres',
-    password: '123456'
-  },
+  connection: db,
   pool: {
     min: 2,
     max: 10
