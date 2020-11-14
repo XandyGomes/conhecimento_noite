b8850e651e44faabec76c659ca169033c556a9e9
XandyGomes <x3.computadores@hotmail.com>
2020-11-13 21:22:12 -0300

    13.11.2020

---
+++ frontend/src/components/home/Stat.vue
@@ -0,0 +1,18 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "Stat",
+  props: ['title', 'value', 'icon', 'color'],
+  computed: {
+    style()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
