7723434519be816dab978a34a473c1b155b8225f
XandyGomes <x3.computadores@hotmail.com>
2020-11-13 22:02:42 -0300

    13.11.2020

--- frontend/src/components/home/Stat.vue
+++ frontend/src/components/home/Stat.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <div class="stat">
+    <div class="stat-icon">
+      <i :class="icon" :style="style"></i>
+    </div>
+    <div class="stat-info">
+      <span class="Stat-title"> {{ title }} </span>
+      <span class="Stat-value"> {{ value }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,12 +15,19 @@ export default {
   name: "Stat",
   props: ['title', 'value', 'icon', 'color'],
   computed: {
-    style()
+    style(){
+      return "color: " + (this.color || "#000")
+    }
   }
 
 }
 </script>
 
 <style>
+  .stats {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
 
 </style>
