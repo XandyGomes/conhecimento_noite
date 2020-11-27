e3582f3965fe2132e9694a644427465541691d73
XandyGomes <x3.computadores@hotmail.com>
2020-11-27 20:36:58 -0300

    27.11.2020

---
+++ frontend/src/components/admin/UserAdmin.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="user-admin">
+    <h1>Teste de Usuário Componente</h1>
+  </div>
+</template>
+
+<script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+
+export default {
+  name: 'UserAdmin',
+  data: function(){
+    return{
+      user: {},
+      users: []
+    }
+  },
+
+  methods: {
+    loadUsers() {
+      const url = `${baseApiUrl}/users`
+      axios.get(url).then(res => {
+        this.users = res.data
+      })
+    }
+  },
+  
+  mounted() {
+    this.loadUsers()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,8 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkFsZXhhbmRyZSBHb21lcyBkYSBTaWx2YSIsImVtYWlsIjoiYWxleGFuZHJlLnNpbHZhMTI0QGZhdGVjLnNwLmdvdi5iciIsImFkbWluIjp0cnVlLCJpYXQiOjE2MDY1MTgxNjMsImV4cCI6MTYwNjYwNDU2M30.EiTeXA5KnMTPGQLLPEtm2glZ08lav1u7lthcekO9S1U'
+
 new Vue({
   store,
   router,
